debacfbd6125992964b0a183221b1b73412031c5
ChrisElliott <chrisae456@gmail.com>
2024-01-17 11:52:12 -0500

    initializing personal project

---
+++ src/main/java/com/ss/StoryStudioApp/StoryStudioAppApplication.java
@@ -0,0 +1,13 @@
+package com.ss.StoryStudioApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryStudioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryStudioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/StoryStudioApp/StoryStudioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.StoryStudioApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryStudioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
